b710d0af78e8936b21689cc2d8d11a32d1434e19
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-10-24 16:54:45 -0500

    Shows lesson on page; WILL CHANGE WHEN REACT ROUTER IS ADDED

--- client/src/components/lesson.js
+++ client/src/components/lesson.js
@@ -3,11 +3,33 @@ import Header from './header.js';
 import Listen from './listen.js';
 import Section from './section.js';
 import Next from './next.js';
+import { useState, useEffect } from 'react';
 
 const Lesson = () => {
+    const [lessons, setLessons] = useState([]);
+
+    const loadLessons = () =>{
+        // A function to fetch the list of students that will be load anytime that list change
+        fetch("/lessons")
+          .then((response) => response.json())
+          .then((lessons) => {
+                setLessons(lessons);
+                console.log(lessons)
+              });
+      }
+
+      useEffect(() => {
+        loadLessons();
+      }, []);
+
   return (
     <div>
-        <p>Lesson is present</p>
+        <h2>Lesson is present</h2>
+        {lessons.map((lesson) => {
+            return (
+                <p>Lesson title: {lesson.title}</p>
+            )
+        })}  
         <Header />
         <Listen />
         <Section />
