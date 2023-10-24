245663f8b3a1054b8a6e22f0652acc585ccf057c
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-10-24 16:17:49 -0500

    Adds listen component

--- client/src/components/lesson.js
+++ client/src/components/lesson.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './header.js';
+import Listen from './listen.js';
 
 const Lesson = () => {
   return (
     <div>
         <p>Lesson is present</p>
         <Header />
+        <Listen />
     </div>
   );
 };
