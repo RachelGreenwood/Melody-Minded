fd5800fda60b581dddb686cb19f75fa6d0d0aa4e
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-10-24 16:26:17 -0500

    Adds Comment component

---
+++ client/src/components/comment.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Comment = () => {
+  return (
+    <div>
+        <p>Comment is present</p>
+    </div>
+  );
+};
+
+export default Comment;
--- client/src/components/forum.js
+++ client/src/components/forum.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AddComment from './addComment';
+import Comment from './comment';
 
 const Forum = () => {
   return (
     <div>
         <p>Forum is present</p>
         <AddComment />
+        <Comment />
     </div>
   );
 };
