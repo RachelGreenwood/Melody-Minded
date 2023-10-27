12f09c1dabf07aa4c503296685b6d60a421ca972
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-10-27 17:00:37 -0500

    Fixing errors: the final episode

--- server/migrations/sqls/20221113192843-initial-down.sql
+++ server/migrations/sqls/20221113192843-initial-down.sql
@@ -1,3 +1,7 @@
 /* Replace with your SQL commands */
 
 DROP TABLE lessons;
+
+DROP TABLE users;
+
+DROP TABLE comments;
--- server/migrations/sqls/20221113192843-initial-up.sql
+++ server/migrations/sqls/20221113192843-initial-up.sql
@@ -12,4 +12,24 @@ CREATE TABLE public.lessons (
     correct_feedback1 text
 );
 
+CREATE TABLE users (
+    id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    username text,
+    email character varying,
+    password character varying,
+    photo character varying,
+    lessons_completed integer
+);
+
+CREATE TABLE comments (
+    id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    poster text,
+    datetime timestamp without time zone,
+    comment text
+);
+
 INSERT INTO lessons (title, concept1, question1, "wrong_answer1A", "wrong_answer1B", "wrong_answer1C", correct_answer1, incorrect_feedback1, correct_feedback1) VALUES ('testlesson', 'first concept', 'one question: ?', 'nop', 'wrong', 'nuh uhh', 'correct', 'um...no', 'nice!');
+
+INSERT INTO users (username, email, password, photo, lessons_completed) VALUES ('Brie', 'brie@idebug.com', 'debuggingqueen', 'yes', 1);
+
+INSERT INTO comments (poster, datetime, comment) VALUES ('Brie', NULL, 'Hello World');
