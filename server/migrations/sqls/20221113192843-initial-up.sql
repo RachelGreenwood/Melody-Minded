590bdbccfa1d5908b0b43c4deee667cfd41bd4c7
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-10-27 16:51:12 -0500

    Fixing errors: el fin de siecle

--- server/migrations/sqls/20221113192843-initial-up.sql
+++ server/migrations/sqls/20221113192843-initial-up.sql
@@ -13,6 +13,3 @@ CREATE TABLE public.lessons (
 );
 
 INSERT INTO lessons (title, concept1, question1, "wrong_answer1A", "wrong_answer1B", "wrong_answer1C", correct_answer1, incorrect_feedback1, correct_feedback1) VALUES ('testlesson', 'first concept', 'one question: ?', 'nop', 'wrong', 'nuh uhh', 'correct', 'um...no', 'nice!');
-
-
-SELECT pg_catalog.setval('students_id_seq', 16, true);
