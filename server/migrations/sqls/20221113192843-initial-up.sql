242a45a200b1d4fed58b84d1d0ac50e0ebcdfce8
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-10-27 16:43:22 -0500

    Fixing errors: the trilogy

---
+++ server/migrations/sqls/20221113192843-initial-up.sql
@@ -0,0 +1,18 @@
+/* Replace with your SQL commands */
+CREATE TABLE public.lessons (
+    id integer NOT NULL,
+    title text,
+    concept1 text,
+    question1 text,
+    "wrong_answer1A" text,
+    "wrong_answer1B" text,
+    "wrong_answer1C" text,
+    correct_answer1 text,
+    incorrect_feedback1 text,
+    correct_feedback1 text
+);
+
+INSERT INTO lessons (title, concept1, question1, "wrong_answer1A", "wrong_answer1B", "wrong_answer1C", correct_answer1, incorrect_feedback1, correct_feedback1) VALUES ('testlesson', 'first concept', 'one question: ?', 'nop', 'wrong', 'nuh uhh', 'correct', 'um...no', 'nice!');
+
+
+SELECT pg_catalog.setval('students_id_seq', 16, true);
