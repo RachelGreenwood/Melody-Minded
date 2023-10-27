baef43516e1778d4c02d4c36194aaab63a506e6e
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-10-26 21:49:15 -0500

    Turns answers into buttons

--- client/src/components/section.js
+++ client/src/components/section.js
@@ -9,10 +9,10 @@ const Section = (props) => {
                 <div key={lesson.id}>
                     <p>Concept: {lesson.concept1}</p>
                     <p>Question: {lesson.question1}</p>
-                    <p>Answer 1: {lesson.wrong_answer1A}</p>
-                    <p>Answer 2: {lesson.wrong_answer1B}</p>
-                    <p>Answer 3: {lesson.wrong_answer1C}</p>
-                    <p>Answer 4: {lesson.correct_answer1}</p>
+                    <button>Answer 1: {lesson.wrong_answer1A}</button>
+                    <button>Answer 2: {lesson.wrong_answer1B}</button>
+                    <button>Answer 3: {lesson.wrong_answer1C}</button>
+                    <button>Answer 4: {lesson.correct_answer1}</button>
                     <p>Feedback if Wrong: {lesson.incorrect_feedback1}</p>
                     <p>Feedback if Right: {lesson.correct_feedback1}</p>
                 </div>
