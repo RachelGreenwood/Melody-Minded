f96966dabb0146a6e36aa17e1b54236d19a1e942
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-10-30 12:34:55 -0500

    Tests that Section component renders

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,11 +1,16 @@
 import { render } from '@testing-library/react';
 import App from './App';
 import Lesson from './components/lesson.js';
+import Section from './components/section.js';
 
-test('renders App component', () => {
+test('App component renders', () => {
   render(<App />);
 });
 
-test('renders Lesson component', () => {
+test('Lesson component renders', () => {
   render(<Lesson />);
+});
+
+test('Section component renders', () => {
+  render(<Section />);
 });
