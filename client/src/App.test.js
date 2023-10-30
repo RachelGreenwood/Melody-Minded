916b10c4893225d1719f8419c40d99f2134271e2
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-10-30 12:21:33 -0500

    Tests that lesson component renders

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,6 +1,11 @@
 import { render } from '@testing-library/react';
 import App from './App';
+import Lesson from './components/lesson.js';
 
 test('renders App component', () => {
   render(<App />);
+});
+
+test('renders Lesson component', () => {
+  render(<Lesson />);
 });
