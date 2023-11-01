62ebde4d24912b38c4ba24f76bd1e9ffe4b21fe6
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-11-01 16:36:42 -0500

    Adds GET request to show one lesson

--- server/server.js
+++ server/server.js
@@ -143,6 +143,7 @@ app.get('/lessons', async (req, res) => {
 app.get('/lessons_new', async (req, res) => {
   try {
     const { rows: lessons } = await db.query(`SELECT
+    lessons_new.id,
     title,
     concept,
     questions.question,
@@ -171,36 +172,40 @@ app.get('/lessons_new', async (req, res) => {
   }
 });
 
-// SELECT title, concept, questions.question, questions."wrong_answerA", questions."wrong_answerB", questions."wrong_answerC", questions.correct_answer, questions.incorrect_feedback, questions.correct_answer
-// FROM questions
-// INNER JOIN concepts
-// ON questions.concept_id = concepts.id
-// INNER JOIN lessons_new
-// ON concepts.lesson_id = lessons_new.id
-// ORDER BY title, concept ASC;
-
-// app.get('/sightings', async (req, res) => {
-//   try {
-//       const { rows: animals } = await db.query(`SELECT
-//       s.id,
-//       s.datetime,
-//       s.species,
-//       s.location,
-//       s.healthy,
-//       s.sighter_email,
-//       s.timestamp AS sighting_timestamp,
-//       a.nickname
-//   FROM
-//       sightings s
-//   LEFT JOIN
-//       animals a
-//   ON
-//       s.species = a.species`);
-//       res.send(animals);
-//   } catch (e) {
-//       return res.status(400).json({ e });
-//   }
-// });
+app.get('/lessons_new/:lessonId', async (req, res) => {
+  try {
+    const { lessonId } = req.params;
+    console.log("lesson id: ", lessonId);
+    const { rows: lessons } = await db.query(`SELECT
+    lessons_new.id,
+    title,
+    concept,
+    questions.question,
+    questions."wrong_answerA",
+    questions."wrong_answerB",
+    questions."wrong_answerC",
+    questions.correct_answer,
+    questions.incorrect_feedback,
+    questions.correct_feedback
+  FROM
+    questions
+  INNER JOIN
+    concepts
+  ON
+    questions.concept_id = concepts.id
+  INNER JOIN
+    lessons_new
+  ON
+    concepts.lesson_id = lessons_new.id
+  WHERE
+    lessons_new.id = $1`, [lessonId]);
+  console.log("In the server, ", lessons);
+  res.send(lessons);
+  } catch(err) {
+    console.log(err);
+    return res.status(400).json({err});
+  }
+});
 
 // Shows server is running
 app.listen(PORT, () => {
