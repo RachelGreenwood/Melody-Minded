65cccd76f81761a8772eaa0299111c36a033d3e2
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-11-02 17:47:32 -0500

    Takes underlining off navbar

--- client/src/components/Home Page/navbar.js
+++ client/src/components/Home Page/navbar.js
@@ -26,14 +26,14 @@ const Navbar = () => {
         <nav>
           <img src={Logo} alt='Melody Minded logo'></img>
           <ul>
-            <li><Link to="/"><div>Home</div></Link></li>
+            <li><Link style={{textDecoration: 'none'}} to="/"><div>Home</div></Link></li>
             {/* Displays all lesson titles as links to each lesson */}
             {lessons_new.map((lesson, index) => (
               <li key={index}>
-                <Link to={`/lessons/${index}`}><div>{lesson.title}</div></Link>
+                <Link style={{textDecoration: 'none'}} to={`/lessons/${index}`}><div>{lesson.title}</div></Link>
               </li>
             ))}
-            <li><Link to="/forum"><div>Forum</div></Link></li>
+            <li><Link style={{textDecoration: 'none'}} to="/forum"><div>Forum</div></Link></li>
           </ul>
         </nav>
     </div>
