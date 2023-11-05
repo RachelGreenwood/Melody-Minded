e684c192613cf3d88b42daf693b7c1d080c8e855
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-11-05 15:28:02 -0600

    Styles color of all link buttons

--- client/src/components/Home Page/navbar.js
+++ client/src/components/Home Page/navbar.js
@@ -28,14 +28,14 @@ const uniqueTitles = [...new Set(lessons_new.map((lesson) => lesson.title))];
         <nav>
           <img src={Logo} alt='Melody Minded logo'></img>
           <ul>
-            <li><Link style={{textDecoration: 'none'}} to="/"><div>Home</div></Link></li>
+            <li><Link className='link' to="/"><div>Home</div></Link></li>
             {/* Displays all lesson titles as links to each lesson */}
             {uniqueTitles.map((lesson, index) => (
               <li key={index + 1}>
-                <Link style={{textDecoration: 'none'}} to={`/lessons/${index + 1}`}><div>{lesson}</div></Link>
+                <Link className='link' to={`/lessons/${index + 1}`}><div>{lesson}</div></Link>
               </li>
             ))}
-            <li><Link style={{textDecoration: 'none'}} to="/forum"><div>Forum</div></Link></li>
+            <li><Link className='link' to="/forum"><div>Forum</div></Link></li>
           </ul>
         </nav>
     </div>
--- client/src/components/Lesson Page/next.js
+++ client/src/components/Lesson Page/next.js
@@ -4,14 +4,15 @@ import { Link } from 'react-router-dom';
 const Next = (props) => {
   const lessonsLength = [...new Set(props.lesson.map((lesson) => lesson.title))].length;
   const nextLessonId = parseInt(props.lessonId) + 1;
+  
   return (
     <div>
         {/* If there are more lessons, link to next lesson */}
         {nextLessonId === lessonsLength ? (
-        <button><Link style={{textDecoration: "none"}} to={`/lessons/${nextLessonId}`}>Next Lesson</Link></button>
+        <button><Link className='link' to={`/lessons/${nextLessonId}`}>Next Lesson</Link></button>
       ) : (
         // If on the last lesson, link to forum page
-        <button><Link style={{textDecoration: "none"}} to={`/forum`}>Forum</Link></button>
+        <button><Link className='link' to={`/forum`}>Forum</Link></button>
       )}
     </div>
   );
