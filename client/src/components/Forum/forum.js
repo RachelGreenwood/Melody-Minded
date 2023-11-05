c102e38a530ddeae85c1998ed44097ed2fc5026e
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-11-05 15:57:59 -0600

    Shows comments on front-end

--- client/src/components/Forum/forum.js
+++ client/src/components/Forum/forum.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AddComment from './addComment';
 import Comment from './comment';
 
 const Forum = () => {
+  const [comments, setComments] = useState([]);
+  const loadComments = () => {
+    fetch(`/comments`)
+      .then((response) => response.json())
+      .then((comments) => {
+        setComments(comments)
+        console.log(comments);
+      });
+  }
+
+  useEffect(() => {
+    loadComments();
+  }, []);
+
   return (
     <div>
         <p>Forum is present</p>
+        {comments.map((comment, index) => {
+          return (
+            <div key={index}>
+              {comment.poster}
+              {comment.comment}
+            </div>
+          )
+        })}
         <AddComment />
         <Comment />
     </div>
