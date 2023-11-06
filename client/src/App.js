8e3a2fdec9c35a63a72bb71cacadcf0fc8e357d8
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-11-06 17:12:27 -0600

    Refactors App.js code

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import Profile from "./components/Authentication/profile";
 import { useAuth0 } from '@auth0/auth0-react';
 import Loading from "./components/Authentication/loading";
 import { Route, Routes, Link } from 'react-router-dom';
-import { useState, useEffect } from 'react';
 import AuthenticationButton from './components/Authentication/authentication-button';
 import Navbar from './components/Home Page/navbar.js';
 import Lesson from './components/Lesson Page/lesson.js';
@@ -19,30 +18,28 @@ function App() {
   }
 
   return (
-    <div id="app" className="d-flex flex-column h-100">
-        <div className="container flex-grow-1">
-          <div className="navbar-grid">
-            <Navbar />
-          </div>
-        <div className="lesson-grid">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="api/me" element={<Profile user={user}/>} />
-            <Route path="lessons/:lessonId" element={<Lesson />} />
-            <Route path='/forum' element={<Forum />} />
-          </Routes>
-        </div>
-        <div className="last-column">
-          <div className="user-info">
-            {/* User info and sign-in button */}
-          {!user ? <span>Please sign in</span> : (
-            <>
-              <img className="avatar" src={user.picture} alt='user avatar' />
-              <span className="auth"><Link className="link" to="api/me">{user.nickname}</Link></span>
-            </>
-          )}
-          <AuthenticationButton />
-          </div>
+    <div id="app" className="container d-flex flex-column h-100">
+      <div className="navbar-grid">
+        <Navbar />
+      </div>
+      <div className="lesson-grid">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="api/me" element={<Profile user={user}/>} />
+          <Route path="lessons/:lessonId" element={<Lesson />} />
+          <Route path='/forum' element={<Forum />} />
+        </Routes>
+      </div>
+      <div className="last-column">
+        {/* User info and sign-in button */}
+        <div className="user-info">
+        {!user ? "" : (
+          <>
+            <img className="avatar" src={user.picture} alt='user avatar' />
+            <span className="auth"><Link className="link" to="api/me">{user.nickname}</Link></span>
+          </>
+        )}
+        <AuthenticationButton />
         </div>
       </div>
     </div>
