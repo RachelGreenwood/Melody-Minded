f5be8fc79b017e7463220db6e0898c0b737730d4
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-11-07 09:51:57 -0600

    Creates form to add a comment

--- client/src/components/Forum/addComment.js
+++ client/src/components/Forum/addComment.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 // Lets user add a comment
 const AddComment = () => {
+  
   return (
     <div>
-        <p>AddComment is present</p>
+        <form>
+          <input type='text' required></input>
+          <button type='submit'>Submit</button>
+        </form>
     </div>
   );
 };
