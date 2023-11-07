110a4901323a416a94a3d41e0ae419cac4024c37
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-11-07 09:28:42 -0600

    Shows timestamp of comment

--- client/src/components/Forum/comment.js
+++ client/src/components/Forum/comment.js
@@ -3,10 +3,20 @@ import React from 'react';
 // Each individual comment
 const Comment = (props) => {
   const { comment } = props;
+  const date = comment.datetime
+    ? new Date(comment.datetime).toLocaleString(undefined, {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+      })
+    : "";
+  
   return (
     <div>
         <p>{comment.poster}</p>
-        <p>{comment.datetime}</p>
+        <p>{date}</p>
         <p>{comment.comment}</p>
     </div>
   );
