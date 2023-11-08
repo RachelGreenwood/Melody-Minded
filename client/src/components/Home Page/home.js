913b7de2cf64acfcdaca7301a0904c9d3285e00d
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-11-08 11:22:43 -0600

    Adds mission to home page

--- client/src/components/Home Page/home.js
+++ client/src/components/Home Page/home.js
@@ -7,6 +7,10 @@ const Home = () => {
     <div>
         <h1>Welcome to Melody Minded!</h1>
         <img src={choir_concert} alt='A photo of a choral concert'></img>
+        <p>Melody Minded is a music theory education application for students ages 5-15 that uses quuizzes to teach musical concepts.</p>
+        <p>As a former Choir Director, I believe that music should be accessible to everyone, regardless of location, economic status, or skill level.</p>
+        <p>Start with lesson one and work your way through the curriculum, then leave your comments and questions in the forum to connect with other users. Make sure you're practicing the concepts you learn by singing wherever and whenever you can!</p>
+        <p>Click "lesson one" in the navigation bar to get started!</p>
     </div>
   );
 };
