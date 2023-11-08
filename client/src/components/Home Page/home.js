1b77174ad95385b3837933347afd97535180013c
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-11-08 12:37:41 -0600

    Changes image position and sizing

--- client/src/components/Home Page/home.js
+++ client/src/components/Home Page/home.js
@@ -7,23 +7,23 @@ import './home.css';
 // Landing page
 const Home = () => {
   const images = [choir_concert, choir2, choir3];
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [ImageIndex, setImageIndex] = useState(0);
 
   const nextImage = () => {
-    setCurrentImageIndex((currentImageIndex + 1) % images.length);
+    setImageIndex((ImageIndex + 1) % images.length);
   };
 
   const previousImage = () => {
-    setCurrentImageIndex((currentImageIndex - 1 + images.length) % images.length);
+    setImageIndex((ImageIndex - 1 + images.length) % images.length);
   };
 
   return (
     <div>
         <h1>Welcome to Melody Minded!</h1>
         <div id='concert-photos'>
-        <i onClick={previousImage} className="arrow-button left-arrow" class="fa-solid fa-caret-left fa-2xl"></i>
-        <img className="concert" src={images[currentImageIndex]} alt="A photo of a choral concert" />
-        <i onClick={nextImage} className="arrow-button right-arrow" class="fa-solid fa-caret-right fa-2xl"></i>
+          <i onClick={previousImage} className="arrow-button left-arrow" class="fa-solid fa-caret-left fa-2xl"></i>
+          <img className="concert" src={images[ImageIndex]} alt="A photo of a choral concert" />
+          <i onClick={nextImage} className="arrow-button right-arrow" class="fa-solid fa-caret-right fa-2xl"></i>
         </div>
         <div id='mission'>
           <p>Melody Minded is a music theory education application for students ages 5-15 that uses quizzes to teach musical concepts.</p>
